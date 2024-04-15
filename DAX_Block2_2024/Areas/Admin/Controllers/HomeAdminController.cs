1953be5f172d2a566ca0b32f59d7c78a31d221e5
Haivan <83487199+haivan552003@users.noreply.github.com>
2024-04-16 05:14:31 +0700

    BE - Add Home Admin

---
+++ DAX_Block2_2024/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAX_Block2_2024.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
